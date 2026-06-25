57c70476b30ea2c258ba5f43b5df1c4eb2da8dfe
Paulo Eduardo <pa4online@gmail.com>
2026-06-25 12:09:00 -0300

    commiti inicial

---
+++ main.py
@@ -0,0 +1,13 @@
+genero_favorito = input()
+genero_musica = input()
+
+def classificar_musica(genero_favorito, genero_musica):
+    if genero_favorito == genero_musica:
+        return 'recomendada'
+    elif genero_favorito == 'Pop' or genero_favorito == 'Rock':
+        return 'neutra'
+    else:
+        return 'não recomendada'
+
+resultado = classificar_musica('Rock', 'Pop')
+print(resultado)
